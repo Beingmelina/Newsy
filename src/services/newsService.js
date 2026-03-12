9173403aaf3efb1321aa447c8b6b5f1458cd31af
Melina Mitri <melina.mitri@gmail.com>
2026-03-12 13:18:01 +0400

    Extend news cache from 5 to 30 minutes

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -6,7 +6,7 @@ const parser = new Parser();
 
 let cachedNews = null;
 let cacheTimestamp = null;
-const CACHE_DURATION = 5 * 60 * 1000;
+const CACHE_DURATION = 30 * 60 * 1000;
 
 let translationClient = null;
 function getTranslationClient() {
